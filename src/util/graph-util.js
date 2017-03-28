ba7a0b75b3fce0e65c2922253779eae341317405
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-03-28 11:30:43 -0400

    Convert to ES6 class syntax

--- src/util/graph-util.js
+++ src/util/graph-util.js
@@ -15,13 +15,14 @@ var ALL_STATEMENTS = null
 /**
  * Appends RDF statements from one graph object to another
  * @method appendGraph
+ *
  * @param toGraph {Graph} Graph object to append to
  * @param fromGraph {Graph} Graph object to append from
- * @param docURI {String} Document URI to use as source
+ * @param docURI {string} Document URI to use as source
  */
 function appendGraph (toGraph, fromGraph, docURI) {
   fromGraph.statementsMatching(ALL_STATEMENTS)
-    .forEach(function (st) {
+    .forEach((st) => {
       toGraph.add(st.subject, st.predicate, st.object, st.why)
     })
 }
@@ -30,31 +31,37 @@ function appendGraph (toGraph, fromGraph, docURI) {
  * Converts a list of RDF statements into a Graph, and returns
  * it.
  * @method graphFromStatements
+ *
  * @param rdf {RDF} RDF library such as rdflib.js
  * @param statements {Array<Statement>}
+ *
  * @return {Graph}
  */
 function graphFromStatements (rdf, statements) {
-  var graph = rdf.graph()
-  statements.forEach(function (st) {
-    graph.addStatement(st)
-  })
+  let graph = rdf.graph()
+
+  statements.forEach((st) => { graph.addStatement(st) })
+
   return graph
 }
 
 /**
  * Parses a given graph, from text rdfSource, as a given content type.
  * Returns parsed graph.
+ *
  * @method parseGraph
  * @param rdf {RDF} RDF library such as rdflib.js
- * @param baseUrl {String}
- * @param rdfSource {String} Text source code
- * @param contentType {String} Mime Type (determines which parser to use)
- * @return {rdf.Graph}
+ * @param baseUrl {string}
+ * @param rdfSource {string} Text source code
+ * @param contentType {string} Mime Type (determines which parser to use)
+ *
+ * @return {Graph}
  */
 function parseGraph (rdf, baseUrl, rdfSource, contentType) {
-  var parsedGraph = rdf.graph()
+  let parsedGraph = rdf.graph()
+
   rdf.parse(rdfSource, parsedGraph, baseUrl, contentType)
+
   return parsedGraph
 }
 
@@ -62,12 +69,14 @@ function parseGraph (rdf, baseUrl, rdfSource, contentType) {
  * Extracts the URIs from a parsed graph that match parameters.
  * The URIs are a set (duplicates are removed)
  * @method parseLinks
+ *
  * @param graph {Graph}
  * @param subject {NamedNode}
  * @param predicate {NamedNode}
  * @param object {NamedNode}
  * @param source {NamedNode}
- * @return {Array<String>} Array of link URIs that match the parameters
+ *
+ * @return {Array<string>} Array of link URIs that match the parameters
  */
 function parseLinks (graph, subject, predicate, object, source) {
   var links = {}
@@ -83,8 +92,10 @@ function parseLinks (graph, subject, predicate, object, source) {
  * Serializes an array of RDF statements into a simple N-Triples format
  * suitable for writing to a solid server.
  * @method serializeStatements
+ *
  * @param statements {Array<Statement>} List of RDF statements
- * @return {String}
+ *
+ * @return {string}
  */
 function serializeStatements (statements) {
   var source = statements.map(function (st) { return st.toNT() })
